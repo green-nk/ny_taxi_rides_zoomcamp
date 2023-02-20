c0c0cc17a57d9c368fa5cb57d3d34cc5b7cf56df
green.nk21@gmail.com <green.nk21@gmail.com>
2023-02-20 04:02:54 +0000

    add yellow taxi data

--- models/staging/stg_yellow_trips_data.sql
+++ models/staging/stg_yellow_trips_data.sql
@@ -2,21 +2,22 @@
 
 SELECT
     -- identifier
-    {{ dbt_utils.surrogate_key(['vendorid', 'lpep_dropoff_datetime']) }} as tripid, 
+    {{ dbt_utils.surrogate_key(['vendorid', 'tpep_dropoff_datetime']) }} as tripid, 
     CAST(vendorid AS INTEGER) AS vendorid, 
     CAST(ratecodeid AS INTEGER) AS ratecodeid, 
     CAST(pulocationid AS INTEGER) AS pickup_locationid, 
     CAST(dolocationid AS INTEGER) AS dropoff_locationid, 
 
     -- timestamps
-    CAST(lpep_pickup_datetime AS TIMESTAMP) AS pickup_datetime, 
-    CAST(lpep_dropoff_datetime AS TIMESTAMP) AS dropoff_datetime, 
+    CAST(tpep_pickup_datetime AS TIMESTAMP) AS pickup_datetime, 
+    CAST(tpep_dropoff_datetime AS TIMESTAMP) AS dropoff_datetime, 
 
     -- trip info
     store_and_fwd_flag, 
     CAST(passenger_count AS INTEGER) AS passenger_count, 
     CAST(trip_distance AS NUMERIC) AS trip_distance, 
-    CAST(trip_type AS INTEGER) AS trip_type, 
+    -- yellow cabs are always street-hail
+    1 AS trip_type, 
 
     -- payment info
     CAST(fare_amount AS NUMERIC) AS fare_amount, 
@@ -24,14 +25,14 @@ SELECT
     CAST(mta_tax AS NUMERIC) AS mta_tax, 
     CAST(tip_amount AS NUMERIC) AS tip_amount, 
     CAST(tolls_amount AS NUMERIC) AS tolls_amount, 
-    CAST(ehail_fee AS NUMERIC) AS ehail_fee, 
+    0 AS ehail_fee, 
     CAST(improvement_surcharge AS NUMERIC) AS improvement_surcharge, 
     CAST(total_amount AS NUMERIC) AS total_amount, 
     CAST(payment_type AS INTEGER) AS payment_type, 
     {{ get_payment_type_description('payment_type') }} AS payment_type_description, 
     CAST(congestion_surcharge AS NUMERIC) AS congestion_surcharge
     
-FROM {{ source('staging', 'green_trips_data') }}
+FROM {{ source('staging', 'yellow_trips_data') }}
 WHERE vendorid IS NOT NULL
 -- dbt run --select <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
